36c8736524b14a8a9becb45c628687766bd82f03
Mercurs <33377392+Mercurs@users.noreply.github.com>
2018-06-20 13:42:19 +0400

    __Zzzzzzzzzzzzz

---
+++ Assets/Game/Scripts/MovePlayer.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class MovePlayer : MonoBehaviour {
+
+	[SerializeField] protected float speed = 5.0f;
+	Rigidbody rb;
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Game/Scripts/PivotPlayer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PivotPlayer : MonoBehaviour {
+	Transform player;
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.localPosition = player.transform.localPosition;
+	}
+}
